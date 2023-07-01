30c4d6e4d71123487a15d7b91bf2308a5d1acb12
Akshay Khona <DesertSled800c@gmail.com>
2023-06-30 19:02:55 -0500

    Error handling implemented

--- app/root.jsx
+++ app/root.jsx
@@ -1,19 +1,16 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
+  isRouteErrorResponse,
 } from "@remix-run/react";
-import styles from '~/styles/main.css'
+import styles from "~/styles/main.css";
 import MainNavigation from "./components/MainNavigation";
-
-// export const meta = () => ({
-//   charset: "utf-8",
-//   title: "New Remix App",
-//   viewport: "width=device-width,initial-scale=1",
-// });
 
 export default function App() {
   return (
@@ -37,6 +34,76 @@ export default function App() {
   );
 }
 
-export function links () {
-  return [{rel: `stylesheet`, href:styles}]
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <Meta />
+          <Links />
+          <title>An error occurred!</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main>
+            <h1>An error occurred!</h1>
+            <p>
+              Status: {error.status}
+              <br />
+              {error.data.message}
+            </p>
+            <p>
+              Back to <Link to="/">safety</Link>!
+            </p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+
+  let errorMessage = "Unknown error";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{errorMessage}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function links() {
+  return [{ rel: `stylesheet`, href: styles }];
 }
